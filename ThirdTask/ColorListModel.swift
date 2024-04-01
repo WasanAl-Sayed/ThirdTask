3d1e524526a41e288e2fc205eb5c564b568295b8
WasanAl-Sayed <wasanjehad75@gmail.com>
2024-04-01 12:40:31 +0300

    DefaultUsers used

--- ThirdTask/ColorListModel.swift
+++ ThirdTask/ColorListModel.swift
@@ -1,16 +1,40 @@
 import UIKit
 
 struct ColorListModel {
-    static let colors: [UIColor: String] = [
-        .lightGray: Constants.lightGray,
-        .red: Constants.red,
-        .systemPurple: Constants.purple,
-        .systemPink: Constants.pink,
-        .systemOrange: Constants.orange,
-        .systemTeal: Constants.teal,
-        .systemYellow: Constants.yellow,
-        .systemBlue: Constants.blue,
-        .systemBrown: Constants.brown,
-        .systemCyan: Constants.cyan
-    ]
+    
+    static let colors: [UIColor] = [.lightGray, .red, .systemPurple, .systemPink, .systemOrange, .systemTeal, .systemBlue, .systemCyan, .systemYellow, .systemBrown]
+    static let descreption: [String] = [Constants.lightGray, Constants.red, Constants.purple, Constants.pink, Constants.orange, Constants.teal, Constants.blue, Constants.cyan, Constants.yellow, Constants.brown]
+    
+    static func storeDescreption () {
+        UserDefaults.standard.set(descreption, forKey: "descreption")
+    }
+    
+    static func retreiveDescreption () -> [String] {
+        guard let descreption = UserDefaults.standard.object(forKey: "descreption") as? [String] else {
+            return []
+        }
+        return descreption
+    }
+    
+    static func storeColors () {
+        do {
+            let colorDataArray = try colors.map { try NSKeyedArchiver.archivedData(withRootObject: $0, requiringSecureCoding: false) }
+            UserDefaults.standard.set(colorDataArray, forKey: "colors")
+        } catch {
+            print("Error: Unable to archive colors - \(error)")
+        }
+    }
+    
+    static func retreiveColors () -> [UIColor] {
+        guard let colorDataArray = UserDefaults.standard.array(forKey: "colors") as? [Data] else {
+            return []
+        }
+        do {
+            let colors = try colorDataArray.compactMap { try NSKeyedUnarchiver.unarchivedObject(ofClass: UIColor.self, from: $0) }
+            return colors
+        } catch {
+            print("Error: Unable to unarchive colors - \(error)")
+            return []
+        }
+    }
 }
--- ThirdTask/ViewController.swift
+++ ThirdTask/ViewController.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-    //UI elements
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var contentDescription: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         tableView.dataSource = self
         tableView.delegate = self
     }
@@ -23,10 +23,10 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedRow = Array(ColorListModel.colors)[indexPath.row]
-        let color = selectedRow.key
-        let name = selectedRow.value
-        
+        let descreption = ColorListModel.retreiveDescreption()
+        let colors = ColorListModel.retreiveColors()
+        let color = colors[indexPath.row]
+        let name = descreption[indexPath.row]
         contentView.backgroundColor = color
         contentDescription.text = name
     }
@@ -34,21 +34,20 @@ extension ViewController: UITableViewDelegate {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return ColorListModel.colors.count
+        let colors = ColorListModel.retreiveColors()
+        return colors.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
-        let colorTuple = Array(ColorListModel.colors)[indexPath.row]
-        let color = colorTuple.key
-        let name = color.accessibilityName.capitalized
-        
+        let colors = ColorListModel.retreiveColors()
+        let color = colors[indexPath.row]
+        let name = colors[indexPath.row].accessibilityName.capitalized
         cell.backgroundColor = color
         var content = cell.defaultContentConfiguration()
         content.textProperties.color = .white
         content.text = name.capitalized
         cell.contentConfiguration = content
-        
         return cell
     }
 }
